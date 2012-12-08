0ed44661e4005e00b86de6ea037ae37d494e4646
Mario A Chavez <mario.chavez@gmail.com>
2012-12-08 09:14:08 -0600

    Form errors formating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,19 @@ module ApplicationHelper
     'error' if model.errors[field].any?
   end
 
-  def errors(model, field)
+  def field_errors(model, field)
     if model.errors[field].any?
       return content_tag :span, class: 'help-inline' do
         model.errors[field].join(', ')
+      end
+    end
+  end
+
+  def form_errors(model)
+    if model.errors.any?
+      content_tag :div, class: 'alert alert-error' do
+        content = content_tag :strong, t(:invalid_form)
+        content << content_tag(:span, t(:correct_errors))
       end
     end
   end
