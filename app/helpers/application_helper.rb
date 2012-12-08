7c0df18d09387c97b7295e48737f6f35456be0b7
Mario A Chavez <mario.chavez@gmail.com>
2012-12-08 09:09:13 -0600

    Better error format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def has_errors(model, field)
+    'error' if model.errors[field].any?
+  end
+
+  def errors(model, field)
+    if model.errors[field].any?
+      return content_tag :span, class: 'help-inline' do
+        model.errors[field].join(', ')
+      end
+    end
+  end
 end
